9f22d9ce43c4ca716bca18fc381b10400e0cbcf1
ashashasher <asherniu@gmail.com>
2020-04-14 18:01:44 -0700

    Update my avatar

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -6,13 +6,20 @@ import Image from "react-bootstrap/Image";
 class NavContainer extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+      <Navbar
+        className="p-3"
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        fixed="top"
+      >
         <Navbar.Brand className="d-lg-none">SHIHAO NIU</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Image
             className="d-none d-lg-flex"
-            src="/images/avatar.jpg"
+            src="/images/avatar.PNG"
             alt="Shihao Niu"
             roundedCircle
             width="48"
